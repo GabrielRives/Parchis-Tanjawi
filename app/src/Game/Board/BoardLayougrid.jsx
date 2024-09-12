9d8024a475b35fe5a183d5f33a8c3b3440b83cc1
Eisen34 <gabyrives@hotmail.fr>
2024-09-12 08:09:22 +0200

    refonte board done

--- app/src/Game/Board/BoardLayougrid.jsx
+++ app/src/Game/Board/BoardLayougrid.jsx
@@ -2,119 +2,123 @@
 import React from "react";
 import "./BoardLayoutGrid.scss";
 import { MapInteractionCSS } from "react-map-interaction";
+const cases = [
+  { name: "1", backgroundColor: "beige" },
+  { name: "2", backgroundColor: "beige" },
+  { name: "3", backgroundColor: "beige" },
+  { name: "4", backgroundColor: "beige" },
+  { name: "Secu1", backgroundColor: "#1168d8" },
+  { name: "6", backgroundColor: "beige" },
+  { name: "7", backgroundColor: "beige" },
+  { name: "8", backgroundColor: "beige", borderWidth: "0px 0px 1px 1px " },
+  { name: "9", backgroundColor: "beige", borderWidth: "0px 0px 1px 1px " },
+  { name: "10", backgroundColor: "beige" },
+  { name: "11", backgroundColor: "beige" },
+  { name: "Secu2", backgroundColor: "#1e1e24" },
+  { name: "13", backgroundColor: "beige" },
+  { name: "14", backgroundColor: "beige" },
+  { name: "15", backgroundColor: "beige" },
+  { name: "16", backgroundColor: "beige" },
+  { name: "Secu3", backgroundColor: "#1e1e24" },
+  { name: "yellowHouse1", backgroundColor: "#ffe600" },
+  { name: "yellowHouse2", backgroundColor: "#ffe600" },
+  { name: "yellowHouse3", backgroundColor: "#ffe600" },
+  { name: "yellowHouse4", backgroundColor: "#ffe600" },
+  { name: "yellowHouse5", backgroundColor: "#ffe600" },
+  { name: "yellowHouse6", backgroundColor: "#ffe600" },
+  { name: "yellowHouse7", backgroundColor: "#ffe600", borderWidth: "0px 0px 1px 1px " },
+  { name: "18", backgroundColor: "beige" },
+  { name: "19", backgroundColor: "beige" },
+  { name: "20", backgroundColor: "beige" },
+  { name: "21", backgroundColor: "beige" },
+  { name: "ExitJaune", backgroundColor: "#ffe600" },
+  { name: "23", backgroundColor: "beige" },
+  { name: "24", backgroundColor: "beige" },
+  { name: "25", backgroundColor: "beige", borderWidth: "0px 1px 1px 0px " },
+  { name: "26", backgroundColor: "beige", borderWidth: "0px 1px 1px 0px " },
+  { name: "27", backgroundColor: "beige" },
+  { name: "28", backgroundColor: "beige" },
+  { name: "Secu4", backgroundColor: "#1e1e24" },
+  { name: "30", backgroundColor: "beige" },
+  { name: "31", backgroundColor: "beige" },
+  { name: "32", backgroundColor: "beige" },
+  { name: "33", backgroundColor: "beige" },
+  { name: "Secu5", backgroundColor: "#1e1e24" },
+  { name: "greenHouse1", backgroundColor: "#088112" },
+  { name: "greenHouse2", backgroundColor: "#088112" },
+  { name: "greenHouse3", backgroundColor: "#088112" },
+  { name: "greenHouse4", backgroundColor: "#088112" },
+  { name: "greenHouse5", backgroundColor: "#088112" },
+  { name: "greenHouse6", backgroundColor: "#088112" },
+  { name: "greenHouse7", backgroundColor: "#088112" },
+  { name: "35", backgroundColor: "beige" },
+  { name: "36", backgroundColor: "beige" },
+  { name: "37", backgroundColor: "beige" },
+  { name: "38", backgroundColor: "beige" },
+  { name: "Secu6", backgroundColor: "#088112" },
+  { name: "40", backgroundColor: "beige" },
+  { name: "41", backgroundColor: "beige" },
+  { name: "42", backgroundColor: "beige", borderWidth: "1px 1px 0px 0px " },
+  { name: "43", backgroundColor: "beige", borderWidth: "1px 1px 0px 0px " },
+  { name: "44", backgroundColor: "beige" },
+  { name: "45", backgroundColor: "beige" },
+  { name: "Secu7", backgroundColor: "#1e1e24" },
+  { name: "47", backgroundColor: "beige" },
+  { name: "48", backgroundColor: "beige" },
+  { name: "49", backgroundColor: "beige" },
+  { name: "50", backgroundColor: "beige" },
+  { name: "Secu8", backgroundColor: "#1e1e24" },
+  { name: "redHouse1", backgroundColor: "#ce1e1e", borderWidth: "0px 0px 1px 1px " },
+  { name: "redHouse2", backgroundColor: "#ce1e1e" },
+  { name: "redHouse3", backgroundColor: "#ce1e1e" },
+  { name: "redHouse4", backgroundColor: "#ce1e1e" },
+  { name: "redHouse5", backgroundColor: "#ce1e1e" },
+  { name: "redHouse6", backgroundColor: "#ce1e1e" },
+  { name: "redHouse7", backgroundColor: "#ce1e1e" },
+  { name: "52", backgroundColor: "beige" },
+  { name: "53", backgroundColor: "beige" },
+  { name: "54", backgroundColor: "beige" },
+  { name: "55", backgroundColor: "beige" },
+  { name: "ExitRouge", backgroundColor: "#ce1e1e" },
+  { name: "57", backgroundColor: "beige" },
+  { name: "58", backgroundColor: "beige" },
+  { name: "59", backgroundColor: "beige", borderWidth: "1px 0px 0px 1px " },
+  { name: "60", backgroundColor: "beige", borderWidth: "1px 0px 0px 1px " },
+  { name: "61", backgroundColor: "beige" },
+  { name: "62", backgroundColor: "beige" },
+  { name: "Secu9", backgroundColor: "#1e1e24" },
+  { name: "64", backgroundColor: "beige" },
+  { name: "65", backgroundColor: "beige" },
+  { name: "66", backgroundColor: "beige" },
+  { name: "67", backgroundColor: "beige" },
+  { name: "Secu10", backgroundColor: "#1e1e24" },
+  { name: "blueHouse1", backgroundColor: "#1168d8" },
+  { name: "blueHouse2", backgroundColor: "#1168d8" },
+  { name: "blueHouse3", backgroundColor: "#1168d8" },
+  { name: "blueHouse4", backgroundColor: "#1168d8" },
+  { name: "blueHouse5", backgroundColor: "#1168d8" },
+  { name: "blueHouse6", backgroundColor: "#1168d8" },
+  { name: "blueHouse7", backgroundColor: "#1168d8" },
+  { name: "pito1", borderWidth: "0px 0px 0px 0px " },
+  { name: "pito2", borderWidth: "0px 0px 0px 0px " },
+];
 
-const cases = {
-  1: "case1",
-  2: "case2",
-  3: "case3",
-  4: "case4",
-  5: "caseSecurity1",
-  6: "case6",
-  7: "case7",
-  8: "case8",
-  9: "case9",
-  10: "case10",
-  11: "case11",
-  12: "caseSecurity2",
-  13: "case13",
-  14: "case14",
-  15: "case15",
-  16: "case16",
-  17: "caseSecurity3",
-  17.1: "yellowHouse1",
-  17.2: "yellowHouse2",
-  17.3: "yellowHouse3",
-  17.4: "yellowHouse4",
-  17.5: "yellowHouse5",
-  17.6: "yellowHouse6",
-  17.7: "yellowHouse7",
-  18: "case18",
-  19: "case19",
-  20: "case20",
-  21: "case21",
-  22: "sortieMaisonJaune",
-  23: "case23",
-  24: "case24",
-  25: "case25",
-  26: "case26",
-  27: "case27",
-  28: "case28",
-  29: "caseSecurity4",
-  30: "case30",
-  31: "case31",
-  32: "case32",
-  33: "case33",
-  34: "caseSecurity5",
-  34.1: "greenHouse1",
-  34.2: "greenHouse2",
-  34.3: "greenHouse3",
-  34.4: "greenHouse4",
-  34.5: "greenHouse5",
-  34.6: "greenHouse6",
-  34.7: "greenHouse7",
-  35: "case35",
-  36: "case36",
-  37: "case37",
-  38: "case38",
-  39: "caseSecurity6",
-  40: "case40",
-  41: "case41",
-  42: "case42",
-  43: "case43",
-  44: "case44",
-  45: "case45",
-  46: "caseSecurity7",
-  47: "case47",
-  48: "case48",
-  49: "case49",
-  50: "case50",
-  51: "caseSecurity8",
-  51.1: "redHouse1",
-  51.2: "redHouse2",
-  51.3: "redHouse3",
-  51.4: "redHouse4",
-  51.5: "redHouse5",
-  51.6: "redHouse6",
-  51.7: "redHouse7",
-  52: "case52",
-  53: "case53",
-  54: "case54",
-  55: "case55",
-  56: "sortieMaisonRouge",
-  57: "case57",
-  58: "case58",
-  59: "case59",
-  60: "case60",
-  61: "case61",
-  62: "case62",
-  63: "caseSecurity9",
-  64: "case64",
-  65: "case65",
-  66: "case66",
-  67: "case67",
-  68: "caseSecurity10",
-  68.1: "blueHouse1",
-  68.2: "blueHouse2",
-  68.3: "blueHouse3",
-  68.4: "blueHouse4",
-  68.5: "blueHouse5",
-  68.6: "blueHouse6",
-  68.7: "blueHouse7",
-};
 function Board() {
   // Function to render a section of the board with specific range of case numbers and orientation
+
   const renderSection = (start, end, orientation) => {
     return (
       <div className={`gridSectionA gridSectionB`}>
-        {Object.entries(cases)
-          .filter(([key]) => key >= start && key <= end)
-          .map(([key, value]) => (
+        {cases
+          .slice(start - 1, end) // Obtenir les éléments entre start et end
+          .map((value, index) => (
             <div
-              key={key}
-              id={value}
+              key={index}
+              id={value.name}
               className={`gridCase ${orientation === "vertical" ? "gridCaseVertical" : "gridCaseHorizontal"}`}
+              style={{ backgroundColor: value.backgroundColor, borderWidth: value.borderWidth }} // Applique la couleur de fond ici
             >
-              {value}
+              {value.name} {/* Affiche le nom de la case */}
             </div>
           ))}
       </div>
@@ -124,58 +128,77 @@ function Board() {
   return (
     <MapInteractionCSS>
       <div className="Boardmap">
-        <div className="mainRedHouse"></div>
-        <div className="mainBlueHouse"></div>
-        <div className="mainGreenHouse"></div>
-        <div className="mainYellowHouse"></div>
+        <div className="mainRedHouse">
+          <div className="mainRedHouse__centerSquare">
+            <div className="mainRedHouse__centerSquare__innerColorSquare">
+            </div>
+          </div>
+        </div>
+        <div className="mainBlueHouse">
+          <div className="mainBlueHouse__centerSquare"></div>
+        </div>
+        <div className="mainGreenHouse">
+          <div className="mainGreenHouse__centerSquare"></div>
+        </div>
+        <div className="mainYellowHouse">
+          <div className="mainYellowHouse__centerSquare"></div>
+        </div>
         <div className="gridBody">
           <div className="topSection">
             <div className="nineToSixteen">
               {renderSection(9, 16, "horizontal")} {/* Plus large que haute */}
             </div>
             <div className="yellowHouse">
-              {renderSection(17, 17.7, "horizontal")} {/* Plus large que haute */}
+              {renderSection(17, 24, "horizontal")} {/* Plus large que haute */}
             </div>
             <div className="eighteenTo25">
-              {renderSection(18, 25, "horizontal")} {/* Plus large que haute */}
+              {renderSection(25, 32, "horizontal")} {/* Plus large que haute */}
             </div>
           </div>
 
           <div className="middleSection">
             <div className="middleLeft">
               <div className="twentySixTo33">
-                {renderSection(26, 33, "vertical")} {/* Plus haute que large */}
+                {renderSection(33, 40, "vertical")} {/* Plus haute que large */}
               </div>
               <div className="greenHouse">
-                {renderSection(34, 34.7, "vertical")} {/* Plus haute que large */}
+                {renderSection(41, 48, "vertical")} {/* Plus haute que large */}
                 <div className="thirtyFiveTo42">
-                  {renderSection(35, 42, "vertical")} {/* Plus haute que large */}
+                  {renderSection(49, 56, "vertical")} {/* Plus haute que large */}
                 </div>
               </div>
             </div>
-            <div className="middleSquare"></div>
+            <div className="middleSquare">
+              <div id="ConnectLineA"></div>
+              <div id="ConnectLineB"></div>
+              <div className="middleSquare__InnerSquare">
+                <div id="ConnectLineC"></div>
+                <div id="ConnectLineD"></div>
+                  {renderSection(97,98, "horizontal")}
+              </div>
+            </div>
             <div className="middleRight">
               <div className="oneToEight">
                 {renderSection(1, 8, "vertical")} {/* Plus haute que large */}
               </div>
               <div className="blueHouse">
-                {renderSection(68, 68.7, "vertical")} {/* Plus haute que large */}
+                {renderSection(89, 96, "vertical")} {/* Plus haute que large */}
               </div>
               <div className="sixtyTo67">
-                {renderSection(60, 67, "vertical")} {/* Plus haute que large */}
+                {renderSection(81, 88, "vertical")} {/* Plus haute que large */}
               </div>
             </div>
           </div>
 
           <div className="bottomSection">
             <div className="fourtyThreeTo50">
-              {renderSection(43, 50, "horizontal")} {/* Plus large que haute */}
+              {renderSection(57, 64, "horizontal")} {/* Plus large que haute */}
             </div>
             <div className="redHouse">
-              {renderSection(51, 51.7, "horizontal")} {/* Plus large que haute */}
+              {renderSection(65, 72, "horizontal")} {/* Plus large que haute */}
             </div>
             <div className="fiftyTwoTo59">
-              {renderSection(52, 59, "horizontal")} {/* Plus large que haute */}
+              {renderSection(73, 80, "horizontal")} {/* Plus large que haute */}
             </div>
           </div>
         </div>
